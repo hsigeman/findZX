4d2cd1d67f1f156e8de5c865ff0a029a8bff703e
Bella Sinclair <bsm.sinclair@gmail.com>
2020-06-11 11:40:43 +0200

    outline of code to summarize gencov data before R-plotting

---
+++ code/normalize-gencov.py
@@ -0,0 +1,5 @@
+#read in as df
+#normalize each sample column
+#count number of sample columns, first half is female, other half is male
+#take mean over each sex for each line
+#print file with same format but only "two" sample columns, one for female, one for male
