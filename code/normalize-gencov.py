9dcba96f64bc280730825f33fb278cb395b50c81
Bella Sinclair <bsm.sinclair@gmail.com>
2020-06-12 16:17:42 +0200

    normalizes each individual by it's max value. takes mean over sexes

--- code/normalize-gencov.py
+++ code/normalize-gencov.py
@@ -1,5 +1,24 @@
-#read in as df
-#normalize each sample column
-#count number of sample columns, first half is female, other half is male
-#take mean over each sex for each line
-#print file with same format but only "two" sample columns, one for female, one for male
+import sys
+import pandas as pn
+
+gencov = pn.read_csv(sys.argv[1], header=None, sep='\t')
+
+gmax = gencov.apply(max).tolist()
+
+nr_samples_each_sex = int((len(gmax) - 6)/2)
+
+samples = list(range(6, len(gmax)))
+females = list(range(6, nr_samples_each_sex + 6))
+males = list(range(6 + nr_samples_each_sex, len(gmax)))
+
+norm = gencov.loc[:,samples].div(gencov.loc[:,samples].max())
+mean_f = norm.loc[:,females].mean(axis=1)
+mean_m = norm.loc[:,males].mean(axis=1)
+
+
+mean_fm = pn.concat([mean_f, mean_m], axis=1)
+
+gencov_mean = pn.merge(gencov.loc[:,[0,1,2,3,4,5]],mean_fm, left_index=True, right_index=True)
+
+sys.stdout.write(gencov_mean.to_csv(header=None, index=None, sep='\t'))
+
