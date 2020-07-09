bfafb5e24f194cdd2f8a3df9f8479f6948aae1dc
Bella Sinclair <bsm.sinclair@gmail.com>
2020-07-09 10:52:48 +0200

    changed number of inputs, added info

--- code/calculate_snpCount_windows.R
+++ code/calculate_snpCount_windows.R
@@ -1,15 +1,18 @@
 library(doBy)
 library(data.table)
 
+# Takes a bed file with sites. Counts the number of sites in 1Mbp and 100kbp windows
+# for each sex. Calculates the ratio and difference between sexes.
+
+
 set.seed(999)
 
 source("code/functions.R")
 args <- commandArgs(trailingOnly = TRUE)
 
-species= args[1]
-filename= args[2]
+filename = args[1]
 
-snp=read.table(filename,header=FALSE,fill=TRUE,stringsAsFactor=FALSE)
+snp = read.table(filename,header=FALSE,fill=TRUE,stringsAsFactor=FALSE)
 
 snp <- plyr::rename(snp, c("V1"="sex", "V2"="chr",
                            "V3"="start", "V4"="end"))
@@ -18,6 +21,8 @@ snp <- remove_chr_less_than_1mb(snp)
 
 snp$count <- 1
 
+
+
 snp_1Mb_ranges_count <- count_snp_win(snp, 1000000)
 
 snp_1Mb_ranges_count_wide <- transform_wide(snp_1Mb_ranges_count)
@@ -25,7 +30,7 @@ snp_1Mb_ranges_count_wide <- transform_wide(snp_1Mb_ranges_count)
 snp_1Mb_ranges_count_wide <- calculate_ratio(snp_1Mb_ranges_count_wide)
 snp_1Mb_ranges_count_wide <- calculate_diff(snp_1Mb_ranges_count_wide)
 
-write.table(snp_1Mb_ranges_count_wide, args[3], quote=FALSE, sep="\t", row.names = F, col.names = F, na = "NA")
+write.table(snp_1Mb_ranges_count_wide, args[2], quote=FALSE, sep="\t", row.names = F, col.names = T, na = "NA")
 
 
 
@@ -36,4 +41,4 @@ snp_100kbp_ranges_count_wide <- transform_wide(snp_100kbp_ranges_count)
 snp_100kbp_ranges_count_wide <- calculate_ratio(snp_100kbp_ranges_count_wide)
 snp_100kbp_ranges_count_wide <- calculate_diff(snp_100kbp_ranges_count_wide)
 
-write.table(snp_100kbp_ranges_count_wide, args[4], quote=FALSE, sep="\t", row.names = F, col.names = F, na = "NA")
+write.table(snp_100kbp_ranges_count_wide, args[3], quote=FALSE, sep="\t", row.names = F, col.names = T, na = "NA")
