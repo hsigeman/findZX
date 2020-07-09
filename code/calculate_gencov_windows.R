404a17e1a4ab3392e247bf19ed5b6e6c2438f00a
Bella Sinclair <bsm.sinclair@gmail.com>
2020-07-09 10:48:35 +0200

    added info, header=true

--- code/calculate_gencov_windows.R
+++ code/calculate_gencov_windows.R
@@ -1,17 +1,20 @@
 library(doBy)
 library(data.table)
 
+# Calculates the mean genome of a statistica in 1Mbp and 100kbp windows from 5kbp windows
+# Can calculate the mean of genome coverage or allele diversity (for 5kbp windows)
+
+
 set.seed(999)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-species= args[1]
-filename= args[2]
+filename = args[1]
 
 
 source("code/functions.R")
 
-cov=read.table(filename,header=FALSE,fill=TRUE,stringsAsFactor=FALSE)
+cov = read.table(filename,header=FALSE,fill=TRUE,stringsAsFactor=FALSE)
 cov <- plyr::rename(cov, c("V1"="contig", "V2"="contig_start","V3"="contig_end", 
                            "V4"="chr", "V5"="start", "V6"="end", "V7"="heterogametic", 
                            "V8"="homogametic"))
@@ -23,9 +26,9 @@ cov <- calculate_ratio(cov)
 # Remove outliers
 cov <- remove_outliers(cov)
 
-cov_1Mb_ranges_sum <- count_cov_win(cov, 1000000)
-cov_100kb_ranges_sum <- count_cov_win(cov, 100000)
+mean_1Mb_ranges <- mean_win(cov, 1000000)
+mean_100kb_ranges <- mean_win(cov, 100000)
 
-write.table(cov_1Mb_ranges_sum, args[3], quote=FALSE, sep="\t", row.names = F, col.names = F, na = "NA")
+write.table(mean_1Mb_ranges, args[2], quote=FALSE, sep="\t", row.names = F, col.names = T, na = "NA")
 
-write.table(cov_100kb_ranges_sum, args[4], quote=FALSE, sep="\t", row.names = F, col.names = F, na = "NA")
+write.table(mean_100kb_ranges, args[3], quote=FALSE, sep="\t", row.names = F, col.names = T, na = "NA")
