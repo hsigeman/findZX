6d584bd43a7df3abb5d90ef1175546fe069e3cd3
Bella Sinclair <bsm.sinclair@gmail.com>
2020-07-09 16:32:56 +0200

    fixed calculation error

--- code/calculate_prop_heterozygosity.py
+++ code/calculate_prop_heterozygosity.py
@@ -4,7 +4,7 @@ if len(sys.argv)==1:
 	print("\nCalculates the proportion off heterozygot individuals from a VCF-file and")
 	print("prints sites with at least a certain proportion of heterozygosity. Assumes")
 	print("that the first half of the individuals are the heterogametic sex.\n")
-	print("Call: python3 {python-script} {VCF} {OUT_PREFIX}\n")
+	print("Call: python3 {python-script} {VCF} {OUTFILE}\n")
 	sys.exit()
 elif not len(sys.argv)==3:
 	print("\nERROR: wrong number of input arguments")
@@ -12,11 +12,13 @@ elif not len(sys.argv)==3:
 	sys.exit()
 
 
-prefix_out = sys.argv[2]
+#prefix_out = sys.argv[2]
 
-out_file_09 = open(prefix_out + '.09.bed', 'w')
-out_file_08 = open(prefix_out + '.08.bed', 'w')
-out_file_05 = open(prefix_out + '.05.bed', 'w')
+out_file_05 = open(sys.argv[2], 'w')
+
+#out_file_09 = open(prefix_out + '.09.bed', 'w')
+#out_file_08 = open(prefix_out + '.08.bed', 'w')
+#out_file_05 = open(prefix_out + '.05.bed', 'w')
 
 
 with open(sys.argv[1], 'r') as vcfFile:
@@ -44,17 +46,17 @@ with open(sys.argv[1], 'r') as vcfFile:
 				if info_fields[i].startswith('0/1'):
 					heterogameticSex_heterozygotCount = 1 + heterogameticSex_heterozygotCount
 
-				elif info_fields[i].startswith('0/0') or info_fields[i].startswith('1/1'):
-					hetero_count = 1 + heterogameticSex_count
+				if not info_fields[i].startswith('./.'):
+					heterogameticSex_count = 1 + heterogameticSex_count
 
 			if heterogameticSex_count > 0:
 				heterogameticSex_heterozygotRatio = heterogameticSex_heterozygotCount/heterogameticSex_count
 
-				if heterogameticSex_heterozygotRatio >= 0.9:
-					out_file_09.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'heterogametic', str(heterogameticSex_heterozygotRatio)]) + '\n')
+#				if heterogameticSex_heterozygotRatio >= 0.9:
+#					out_file_09.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'heterogametic', str(heterogameticSex_heterozygotRatio)]) + '\n')
 
-				if heterogameticSex_heterozygotRatio >= 0.8:
-					out_file_08.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'heterogametic', str(heterogameticSex_heterozygotRatio)]) + '\n')
+#				if heterogameticSex_heterozygotRatio >= 0.8:
+#					out_file_08.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'heterogametic', str(heterogameticSex_heterozygotRatio)]) + '\n')
 
 				if heterogameticSex_heterozygotRatio >= 0.5:
 					out_file_05.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'heterogametic', str(heterogameticSex_heterozygotRatio)]) + '\n')
@@ -65,25 +67,25 @@ with open(sys.argv[1], 'r') as vcfFile:
 				if info_fields[i].startswith('0/1'):
 					homogameticSex_heterozygotCount = 1 + homogameticSex_heterozygotCount
 
-				elif info_fields[i].startswith('0/0') or info_fields[i].startswith('1/1'):
+				if not info_fields[i].startswith('./.'):
 					homogameticSex_count = 1 + homogameticSex_count
 
 			if homogameticSex_count > 0:
 				homogameticSex_heterozygotRatio = homogameticSex_heterozygotCount/homogameticSex_count
 
-				if homogameticSex_heterozygotRatio >= 0.9:
-					out_file_09.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'homogametic', str(homogameticSex_heterozygotRatio)]) + '\n')
+#				if homogameticSex_heterozygotRatio >= 0.9:
+#					out_file_09.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'homogametic', str(homogameticSex_heterozygotRatio)]) + '\n')
 
-				if homogameticSex_heterozygotRatio >= 0.8:
-					out_file_08.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'homogametic', str(homogameticSex_heterozygotRatio)]) + '\n')
+#				if homogameticSex_heterozygotRatio >= 0.8:
+#					out_file_08.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'homogametic', str(homogameticSex_heterozygotRatio)]) + '\n')
 
 				if homogameticSex_heterozygotRatio >= 0.5:
 					out_file_05.write('\t'.join(info_fields[0:2] + [str(int(info_fields[1])+1), 'homogametic', str(homogameticSex_heterozygotRatio)]) + '\n')
 
 
 
-out_file_09.close()
-out_file_08.close()
+#out_file_09.close()
+#out_file_08.close()
 out_file_05.close()
 
 
