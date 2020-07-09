90938f881c00559c85691829f0ef35101a178a9f
Bella Sinclair <bsm.sinclair@gmail.com>
2020-07-09 10:59:50 +0200

    cleaned up and added info and error message

--- code/filter_allFreq.py
+++ code/filter_allFreq.py
@@ -2,22 +2,25 @@ import sys
 import pandas as pn
 
 if len(sys.argv)==1:
+	print("Filters an allele frequency file and keeps sites that have a")
+	print("frequency between 0.4 and 0.6. Prints to stdout.")
 	sys.exit()
 elif not len(sys.argv)==3:
+	print("ERROR: wrong number of input arguments")
+	print("Call: python3 {script} {Allele Frequency} {sex}")
 	sys.exit()
 
-allFreq_f = pn.read_csv(sys.argv[1], sep='\t', skiprows=1, header=None)
+allFreq = pn.read_csv(sys.argv[1], sep='\t', skiprows=1, header=None)
 
-allFreq_f.loc[:,4]= allFreq_f.loc[:,4].str.split(':').str[1].astype(float)
-allFreq_f.loc[:,5]= allFreq_f.loc[:,5].str.split(':').str[1].astype(float)
+allFreq.loc[:,4]= allFreq.loc[:,4].str.split(':').str[1].astype(float)
 
-allFreq_fHet = allFreq_f.loc[(allFreq_f.loc[:,4] < 0.6) & (allFreq_f.loc[:,4] > 0.4),:]
+allFreqHet = allFreq.loc[(allFreq.loc[:,4] < 0.6) & (allFreq.loc[:,4] > 0.4),:]
 
-allFreq_fHet = allFreq_fHet.drop(allFreq_fHet.columns[[2,3,4,5]], axis=1)
+allFreqHet = allFreqHet.drop(allFreqHet.columns[[2,3,4,5]], axis=1)
 
-allFreq_fHet['pos_end'] = allFreq_f.loc[:,1]+1
-allFreq_fHet['sex'] = sys.argv[2]
+allFreqHet['pos_end'] = allFreq.loc[:,1]+1
+allFreqHet['sex'] = sys.argv[2]
 
-sys.stdout.write(allFreq_fHet.to_csv(header=None, index=None, sep='\t'))
+sys.stdout.write(allFreqHet.to_csv(header=None, index=None, sep='\t'))
 
 
