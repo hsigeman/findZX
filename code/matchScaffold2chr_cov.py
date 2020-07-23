2cbcc082ddddc9b412ae5631b723043a4314d338
Bella Sinclair <bsm.sinclair@gmail.com>
2020-07-23 17:13:35 +0200

    different number of samples of each sex

--- code/matchScaffold2chr_cov.py
+++ code/matchScaffold2chr_cov.py
@@ -5,20 +5,38 @@ if len(sys.argv)==1:
 	print("Pyton-script written for the matchScaffold2Chr rule in snakemake pipe-line.")
 	print("Normalizes each sample on the mean of that sample and then takes the mean over each sex.\n")
 	sys.exit()
-elif not len(sys.argv)==2:
+elif not len(sys.argv)>=2:
 	print("\nError:\tincorrect number of command-line arguments")
-	print("Syntax:\tmatchScaffold2chr_cov.py [genCov] \n")
+	print("Syntax:\tmatchScaffold2chr_cov.py [genCov] {heterogametic samples. prefix 'het:'}")
+	print("{homogametic samples. prefix 'homo:'} \n")
 	sys.exit()
+
+
+nr_samples = int(len(sys.argv) - 1)
+
+heterogametic_samples = []
+homogametic_samples = []
+
+for i in range(2, len(sys.argv)):
+        sample_args = sys.argv[i].split(':')
+
+        if sys.argv[i].startswith('het'):
+                heterogametic_samples.append(sample_args[1])
+
+        elif sys.argv[i].startswith('homo'):
+                homogametic_samples.append(sample_args[1])
+
+nr_heterogametic = len(heterogametic_samples)
+nr_homogametic = len(homogametic_samples)
+
 
 gencov_ref = pn.read_csv(sys.argv[1], sep='\t', header=None)
 gencov_ref = gencov_ref.drop(gencov_ref.columns[[3, 4, 5, 6, 10, 11, 12]], axis=1)
 
-nr_samples = gencov_ref.apply(max).tolist()
-nr_samples_each_sex = int((len(nr_samples) - 6)/2)
+samples = list(range(13, 13 + nr_samples))
 
-samples = list(range(13, 13 + 2*nr_samples_each_sex))
-heterogametic_sex = list(range(13, 13 + nr_samples_each_sex))
-homogametic_sex = list(range(13 + nr_samples_each_sex, 13 + 2*nr_samples_each_sex))
+heterogametic_sex = list(range(13, 13 + nr_heterogametic))
+homogametic_sex = list(range(13 + nr_heterogametic, 13 + nr_heterogametic + nr_homogametic))
 
 norm = gencov_ref.loc[:,samples].div(gencov_ref.loc[:,samples].mean())	
 mean_hetero = norm.loc[:,heterogametic_sex].mean(axis=1)
